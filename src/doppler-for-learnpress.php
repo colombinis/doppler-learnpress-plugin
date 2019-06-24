623129f08a940da5b198631f815be4cd991c0f8b
hernangalvan <hernangalvan79@gmail.com>
2019-06-24 17:47:53 -0300

    Updated some code comments

--- src/doppler-for-learnpress.php
+++ src/doppler-for-learnpress.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
  *
  * @link              www.fromdoppler.com
  * @since             1.0.0
@@ -30,11 +24,6 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
 define( 'DOPPLER_FOR_LEARNPRESS_VERSION', '1.0.0' );
 define( 'DOPPLER_FOR_LEARNPRESS_URL', plugin_dir_url(__FILE__) );
 
@@ -67,10 +56,6 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-doppler-for-learnpress.php
 
 /**
  * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
  *
  * @since    1.0.0
  */
